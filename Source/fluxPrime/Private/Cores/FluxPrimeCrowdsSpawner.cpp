249d2a97197351a2d6843ee348b738930f2bcf77
aditya <adityamuhammad600@gmail.com>
2026-04-26 03:46:25 +0700

    feat(add content): example content

---
+++ Source/fluxPrime/Private/Cores/FluxPrimeCrowdsSpawner.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Crowds/FluxPrimeCrowdsSpawner.h"
+
+
+AFluxPrimeCrowdsSpawner::AFluxPrimeCrowdsSpawner()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AFluxPrimeCrowdsSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFluxPrimeCrowdsSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/fluxPrime/Public/Cores/FluxPrimeCrowdsSpawner.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FluxPrimeCrowdsSpawner.generated.h"
+
+UCLASS()
+class FLUXPRIME_API AFluxPrimeCrowdsSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFluxPrimeCrowdsSpawner();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
